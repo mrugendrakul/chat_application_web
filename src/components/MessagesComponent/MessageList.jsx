a6eec09b40e130c9a372f2ea485709a2d8af7666
mrugendrakul <mrugendraskulkarni@gmail.com>
2025-08-03 20:07:05 +0530

    Added member senderId to the message

--- src/components/MessagesComponent/MessageList.jsx
+++ src/components/MessagesComponent/MessageList.jsx
@@ -2,18 +2,28 @@ import React from 'react'
 import Message from '../../dataLayer/Message'
 
 const MessageList = ({
+  isGroup,
     message = Message(),
     currentSender
 }) => {
+  console.log("message is",message)
   return (
     <div>
       {!currentSender && 
+      <div className='flex flex-col'>
+        {isGroup && <p className='ml-2 p-0'
+        >{message.senderId}</p>}
       <div
       className='flex flex-row justify-start'>
+        
       <div className='bg-gray-300 text-black
       m-2 p-3 rounded-r-xl
-       rounded-bl-xl rounded-tl-sm max-w-8/10'>
-        {message.content}</div>
+       rounded-bl-xl rounded-tl-sm max-w-8/10 flex flex-col'>
+        {message.content}
+        <p className='mt-2 p-0 text-sm border-t-1 border-t-blue-400'>{message.timeStamp.seconds}</p>
+        
+        </div>
+      </div>
       </div>
       }
 
